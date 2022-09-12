b17ed89ae539529d92659f64cd951b144081f433
dhanushsss <dhanush.sridhar01@gmail.com>
2022-09-12 11:40:51 +0530

    Changes

--- src/main/java/com/example/moviebookingbackend/MovieBookingBackendApplication.java
+++ src/main/java/com/example/moviebookingbackend/MovieBookingBackendApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MovieBookingBackendApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(MovieBookingBackendApplication.class, args);
     }
